063022b6a731393d7a85cbca7fec4f6d30daa902
Cornish <208002021@logon.ds.ge.com>
2019-10-14 13:00:49 -0400

    added new files

---
+++ test_import.py
@@ -0,0 +1,6 @@
+import best_practices
+
+print("Scope = :" + __name__)
+
+print("data=" + (best_practices.read_data_from_web()))
+
--- test_main.py
+++ test_main.py
@@ -1,1 +1,7 @@
-import system
+def main():
+    print("Hello World")
+
+
+# if __name__ == "__main__":
+#    main()
+
